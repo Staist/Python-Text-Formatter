6ae865d45301906eaa133551301dc785602f5b38
Staist <77368284+Staist@users.noreply.github.com>
2021-01-22 02:56:38 +0300

    Update formatter.py

--- formatter.py
+++ formatter.py
@@ -1,15 +1,15 @@
-dosyaadi = input("Dosya adı giriniz: ")
+dosyaadi = input("Enter file name: ")
 dosyaadi = str(dosyaadi + ".txt")
 
 with open(dosyaadi, 'r') as file :
   dosyaicerigi = file.read()
 
-silinecek = str(input("Silinecek yazıyı giriniz: "))
+silinecek = str(input("Enter the text that you wish to delete: "))
 dosyaicerigi = dosyaicerigi.replace(silinecek, '')
 
 with open(dosyaadi, 'w') as file:
   file.write(dosyaicerigi)
   file.close()
   print("-" * 30)
-  print("İşlem başarılı!")
+  print("Successfully deleted!")
   print("-" * 30)
